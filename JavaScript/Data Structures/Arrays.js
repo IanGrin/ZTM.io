2dcc9674c29eb2435875dd9756f424c6e24a8855
Ian Merritt <iangrin.coding@gmail.com>
2023-03-10 18:57:47 -0700

    class work

---
+++ JavaScript/Data Structures/Arrays.js
@@ -0,0 +1,11 @@
+var array = ["Banana", "Apples", "Oranges","Blueberries"];
+
+array.shift();
+array.sort();
+array.push("Kiwi");
+array.splice(0, 1);
+array.reverse();
+
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+array2[1][1][0];
---
+++ JavaScript/Data Structures/Objects.js
@@ -0,0 +1,48 @@
+// var user = {
+//     name: 'John',
+//     age: 34,
+//     hobby: 'Soccer',
+//     isMarried: false,
+//     spells: ["abrakabra", "shazam", "boo"],
+//     shout: function() {
+//         console.log("AHHHHHHH!");
+//     }
+// };
+
+// var list = [
+//     {
+//         username: "Andy",
+//         password: "secret"
+//     },
+//     {
+//         username: "Jess",
+//         password: "123"
+//     }
+// ];
+
+{
+    username: 'iangrin',
+    password: 'jello123'
+};
+
+var database = [
+    {
+        username: 'iangrin',
+        password: 'jello123'
+    }
+];
+
+var newsfeed = [
+    {
+        username: 'Jojo',
+        timeline: 'bizzare adventure'
+    },
+    {
+        username: 'hamlet',
+        timeline: 'ham and toast is his go to'
+    },
+    {
+        username: 'scorpio',
+        timeline: 'he just couldnt remember is astro sign :/'
+    }
+]
