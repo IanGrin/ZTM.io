90c5bc36de55524aa1769eacd70bf2c3f3cd0f72
Ian Merritt <iangrin.coding@gmail.com>
2023-03-14 16:59:31 -0600

    classwork

---
+++ JavaScript/DOM - Document Object Model/DOM Selectors/script2.js
@@ -0,0 +1,26 @@
+var button = document.getElementById("enter");
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
+
+function createListElement() {
+    var div = document.createElement('div');
+    var li = document.createElement('li');
+    var delButton = document.createElement('button');
+    div.classList.add('wrapper');
+    ul.appendChild(div);
+    div.append(li, delButton);
+    li.classList.add('taskClass');
+    li.appendChild(document.createTextNode(input.value));
+    input.value = "";
+    delButton.classList.add("delClass");
+    delButton.innerHTML = 'Del';
+}
+
+function inputLength() {
+    return input.value.length;
+}
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
